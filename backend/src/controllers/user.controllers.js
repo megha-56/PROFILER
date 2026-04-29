019b87a6bcd9d3b8bb359db9a5007091afc5e5c6
megha-56 <meghapanthi435@gmail.com>
2026-04-29 18:17:19 +0530

    addee edit profile controller

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -74,3 +74,28 @@ export const getUserProfile = async (req, res) => {
             res.status(500).json({message: "Server error"});
         }
     }
+
+
+export const editProfile=async (req,res)=>{
+    try{
+       const {name,username,email,password, phoneNo,pfp, gender, dob,bio, skills}= req.body;
+       if(!username||!password){
+          return res.status(400).json({message: "Username and password are required"});
+       }
+       const user = await User.findOne({username}); //matches the username with the saved usernames,save the result in user variable
+        //username
+        if(!user){//if no user found
+            return res.status(400).json({message: "Invalid username or password"}); //return res.status.json
+        }
+        if (username) user.username=username;
+        if (name) user.name=name;
+        if (email) user.email=email;
+        if (phoneNo) user.phoneNo=phoneNo;
+        if (pfp) user.pfp=pfp;
+        if (gender) user.gender=gender;
+        if (dob) user.dob=dob;
+        if (bio) user.bio=bio;
+        if (skills) user.skills=skills;
+        return res.status(200).json({message:"Profile updated successfully",user});
+    }
+}
