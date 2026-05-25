fc42c26e659f8d676a52ed160c82ecbe01efd896
megha-56 <meghapanthi435@gmail.com>
2026-05-25 21:54:07 +0530

    Added a middleware that verifies token

---
+++ backend/src/middleware/user.middleware.js
@@ -0,0 +1,19 @@
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+
+
+const verifyToken= (req,res,next)=>{
+    //get token from the request headers
+    const token=req.headers.authorization?.split("")[1];
+    if(!token){
+        return res.status(401).json({message:"Access denied, No token provided."})
+    }
+    try{
+        const user=json.verify(token,JWT_SECRET);
+        req.user=user;
+        next();
+    }catch(error){
+        return res.status(401).json({message:"Invalid token"});
+    }
+}
+
+export default verifyToken;
