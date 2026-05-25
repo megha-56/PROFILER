d48545ad9480c86d821e6a3c61f9aabfd28a266d
megha-56 <meghapanthi435@gmail.com>
2026-05-25 22:01:23 +0530

    added verifyToken to router

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,14 +2,15 @@
 
 import { Router } from "express"; // step-1: import router
 import { editProfile, getUserProfile, loginUser, registerUser } from "../controllers/user.controllers.js"; //step-4:import all controllers
+import verifyToken from "../middleware/user.middleware.js";
 
 const router = Router(); // step-2: make router
 
 // step-3: router.request_type("/routes",coontroller_function)
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/profile", getUserProfile);
-router.post("/edit", editProfile);
+router.post("/profile",verifyToken, getUserProfile);
+router.post("/edit",verifyToken, editProfile);
 
 export default router; //step-5:export the function
 
